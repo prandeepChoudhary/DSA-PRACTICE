2608af298df1c58955e9cdb1cc37b4ff53c1c3d7
prandeepchoudhary <prandeep28@outlook.com>
2025-03-24 15:57:18 +0530

    Stock Buy and sell  prediction

--- DSA-PRACTICE/Program.cs
+++ DSA-PRACTICE/Program.cs
@@ -7,9 +7,13 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
-        ArrayClass array = new ArrayClass();
-        int[] arr = { 1, 2, 0, 4, 3, 0, 5, 0 };
-        array.RotateArr(arr, 3);
+        //ArrayClass array = new ArrayClass();
+        //int[] arr = { 1, 2, 0, 4, 3, 0, 5, 0 };
+        //array.RotateArr(arr, 3);
+
+        StockSellBuy stockSellBuy = new StockSellBuy();
+        int[] arr = { 7, 1, 5, 3, 8, 4 };
+        stockSellBuy.MaxProfit(arr);
     }
 }
 
---
+++ DSA-PRACTICE/StockSellBuy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace DSA_PRACTICE
+{
+	public class StockSellBuy
+	{
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;  // Start with a very high number
+            int maxProfit = 0;  // Start with no profit
+
+
+            foreach (int price in prices)
+            {
+                if(price < minPrice)
+                {
+                    minPrice = price;
+                }
+                else
+                {
+                    maxProfit = Math.Max(maxProfit, price - minPrice);
+                }
+            }
+                return maxProfit;
+        }
+
+    }
+}
+
